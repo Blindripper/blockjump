d8bc0f170827885b1fe6fa0d585f5c8eacbd0ad4
Blindripper <39067706+Blindripper@users.noreply.github.com>
2024-07-23 15:43:05 +0200

    Update BlockJumpGame.sol

--- src/BlockJumpGame.sol
+++ src/BlockJumpGame.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract BlockJumpGame is ERC721URIStorage {
     using Counters for Counters.Counter;
@@ -17,22 +18,29 @@ contract BlockJumpGame is ERC721URIStorage {
     }
 
     event ScoreSubmitted(address player, string name, uint256 score, uint256 blocksClimbed, uint256 submittedGameStartTime, uint256 storedGameStartTime);
-    event GameTryPurchased(address player, uint256 tries);
+    event GameTryPurchased(address player, uint256 tries, bool usedJump);
+    event JackpotClaimed(address winner, uint256 xtzAmount, uint256 jumpAmount);
+    event FundsAdded(address funder, uint256 xtzAmount, uint256 jumpAmount);
 
     Highscore[10] public highscores;
-    uint256 public contractBalance;
+    uint256 public xtzBalance;
+    uint256 public jumpBalance;
     address public owner;
     mapping(address => uint256) public gameTries;
     mapping(address => uint256) public lastGameStartTime;
     mapping(address => mapping(uint256 => bool)) public achievements;
 
     uint256 public constant TOKEN_VALIDITY_PERIOD = 1 hours;
+    uint256 public constant XTZ_PRICE = 0.01 ether;
+    uint256 public constant JUMP_PRICE = 20 * 10**18; // 20 JUMP tokens
 
     string private _baseTokenURI;
+    IERC20 public jumpToken;
 
-    constructor() ERC721("BlockJumpAchievement", "BJA") {
+    constructor(address _jumpTokenAddress) ERC721("BlockJumpAchievement", "BJA") {
         owner = msg.sender;
         _baseTokenURI = "ipfs://QmZxVUxqQRUUV6hGJo96ik6cStPZJmd6PfdDStQJcKtThn/";
+        jumpToken = IERC20(_jumpTokenAddress);
     }
 
     function _baseURI() internal view override returns (string memory) {
@@ -76,33 +84,53 @@ contract BlockJumpGame is ERC721URIStorage {
 
     function claimPrize() public {
         require(msg.sender == highscores[0].player, "Only the top scorer can claim the prize");
-        uint256 prize = (contractBalance * 90) / 100;
-        uint256 ownerShare = contractBalance - prize;
+        uint256 xtzPrize = (xtzBalance * 90) / 100;
+        uint256 jumpPrize = (jumpBalance * 90) / 100;
+        uint256 ownerXtzShare = xtzBalance - xtzPrize;
+        uint256 ownerJumpShare = jumpBalance - jumpPrize;
         
-        contractBalance = 0;
+        xtzBalance = 0;
+        jumpBalance = 0;
         
-        (bool successPlayer, ) = payable(msg.sender).call{value: prize}("");
-        require(successPlayer, "Failed to send prize to player");
+        (bool successPlayer, ) = payable(msg.sender).call{value: xtzPrize}("");
+        require(successPlayer, "Failed to send XTZ prize to player");
         
-        (bool successOwner, ) = payable(owner).call{value: ownerShare}("");
-        require(successOwner, "Failed to send share to owner");
+        require(jumpToken.transfer(msg.sender, jumpPrize), "Failed to send JUMP prize to player");
         
-        require(address(this).balance == 0, "Contract balance should be zero after prize claim");
+        (bool successOwner, ) = payable(owner).call{value: ownerXtzShare}("");
+        require(successOwner, "Failed to send XTZ share to owner");
+        
+        require(jumpToken.transfer(owner, ownerJumpShare), "Failed to send JUMP share to owner");
+        
+        emit JackpotClaimed(msg.sender, xtzPrize, jumpPrize);
     }
 
-    function purchaseGameTries() public payable {
-        require(msg.value == 0.01 ether, "Incorrect payment amount");
-        gameTries[msg.sender] += 10;
-        contractBalance += msg.value;
-        emit GameTryPurchased(msg.sender, 10);
+    function purchaseGameTries(bool useJump) public payable {
+        if (useJump) {
+            require(jumpToken.transferFrom(msg.sender, address(this), JUMP_PRICE), "JUMP transfer failed");
+            gameTries[msg.sender] += 20;
+            jumpBalance += JUMP_PRICE;
+        } else {
+            require(msg.value == XTZ_PRICE, "Incorrect XTZ payment amount");
+            gameTries[msg.sender] += 10;
+            xtzBalance += msg.value;
+        }
+        emit GameTryPurchased(msg.sender, useJump ? 20 : 10, useJump);
     }
 
     function getGameTries(address player) public view returns (uint256) {
         return gameTries[player];
     }
 
-    function addFunds() public payable {
-        contractBalance += msg.value;
+    function addFunds(uint256 jumpAmount) public payable {
+        if (msg.value > 0) {
+            xtzBalance += msg.value;
+        }
+        if (jumpAmount > 0) {
+            require(jumpToken.transferFrom(msg.sender, address(this), jumpAmount), "JUMP transfer failed");
+            jumpBalance += jumpAmount;
+        }
+        emit FundsAdded(msg.sender, msg.value, jumpAmount);
     }
 
     function mintAchievement(uint256 achievementId) public {
